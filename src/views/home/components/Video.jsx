8fa6da3c6cd8babde622c02c954880d883ae1e4c
leofrontdev <lmartinez@ibl.mx>
2024-12-14 19:51:07 -0600

    video path modified

--- src/views/home/components/Video.jsx
+++ src/views/home/components/Video.jsx
@@ -1,8 +1,10 @@
+import media from "../../../assets/videos/landing_lg.mp4";
+
 function Video() {
   return (
     <article className="w-full h-2/3 relative">
       <video className="w-full h-full object-cover" loop autoPlay muted>
-        <source src="/src/assets/videos/landing_md.mp4" type="video/mp4" />
+        <source src={media} type="video/mp4" />
       </video>
       <div className="w-full h-full absolute left-0 top-0 flex items-end pb-9 bg-dark/60">
         <h1 className="text-3xl p-3 text-light bg-dark/30">
